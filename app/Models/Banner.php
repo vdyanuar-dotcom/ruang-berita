b05242b0544085a04b1d7aef3d06092a9a3bc510
vdyanuar <vincensius2316@gmail.com>
2026-04-11 09:41:58 +0700

    menyelesaikan migrasi database

---
+++ app/Models/Banner.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Banner extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'news_id'
+    ];
+
+    public function news()
+    {
+        return $this->belongsTo(News::class);
+    }
+}
---
+++ app/Models/News.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class News extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'author_id',
+        'news_category_id',
+        'title',
+        'slug',
+        'thumbnail',
+        'content'
+    ];
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function newsCategory()
+    {
+        return $this->belongsTo(newsCategory::class);
+    }
+
+    public function banner()
+    {
+        return $this->hasOne(Banner::class);
+    }
+}
